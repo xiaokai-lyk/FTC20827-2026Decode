deea6be2b550f11298355049fcd419fb345811c7
lucasnotfound59 <lucasnotfound59@gmail.com>
2025-11-20 20:02:51 +0800

    写完了scoring Command的框架

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ScoringCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ScoringCommands.java
@@ -4,6 +4,7 @@ import com.arcrobotics.ftclib.command.CommandGroupBase;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 
+import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Shooter;
 
@@ -17,7 +18,8 @@ public class ScoringCommands {
         INTAKE_BALL,
         TRANSFER_BALL,
         ACCELERATE_SHOOTER,
-        SHOOT_BALL;
+        SHOOT_BALL,
+        RESET;
     }
     public ScoringCommands(
         Intake intake,
@@ -27,18 +29,36 @@ public class ScoringCommands {
         this.intake=intake;
         this.shooter=shooter;
         //this.preShooter=Preshooter;
+
     }
-    public CommandGroupBase scoringAction(Actions action) {
+    public CommandGroupBase ScoringAction(Actions action,Constants.ShooterConfig shooterConfig) {
         switch (action){
             case INTAKE_BALL:
                 return new SequentialCommandGroup(
-                    intake.startIntake()
-                    //preShooter.holdBall()
+                    intake.startIntake(),
+                    shooter.stopPreShooter()
                 );
-            case TRANSFER_BALL:
-
-
+//            case TRANSFER_BALL:
+//                return new SequentialCommandGroup(
+//                    intake.startIntake(),
+//                    shooter.stopPreShooter()
+//                );
+//            暂时不需要分开，底盘移动在TeleOpDriveCommand里
+            case ACCELERATE_SHOOTER:
+                Constants.ShooterConfig finalConfig = shooterConfig != null ?
+                    shooterConfig : new Constants.ShooterConfig(1000, 1000);//为了防止没有传shooterConfig但是使用scoringAction
+                return new SequentialCommandGroup(
+                    shooter.setShooter(shooterConfig)
+                );
+            case SHOOT_BALL:
+                return new SequentialCommandGroup(
+                    shooter.runPreShooter()
+                );
+            case RESET:
+                return new SequentialCommandGroup(
+                    intake.stopIntake()
+                );
         }
-
+        return null;
     }
 }
